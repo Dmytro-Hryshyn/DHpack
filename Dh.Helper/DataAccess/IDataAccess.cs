ced9fef34b60ea123d208997ed915a6c52735ae0
vm <vm@DESKTOP-CKR17CJ>
2021-04-10 21:08:07 +0100

    Start Commit

---
+++ Dh.Helper/DataAccess/IDataAccess.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Dh.Helper.DataAccess
+{
+    public interface IDataAccess
+    {
+        List<T> LoadData<T, U>(string sqlStatement, U parameter, string connectionString);
+        void SaveData<T>(string sqlStatement, T parameters, string connectionString);
+    }
+}
